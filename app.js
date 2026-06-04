bfc56241cf8f61a91b4687390457fba2f62df4ed
s0830105 <s0830105@gmail.com>
2026-06-04 19:43:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCY-ZuOIT0gJsqA5ep1WcMcQpw8Hk_EdpI",
+  authDomain: "s20260627-ea381.firebaseapp.com",
+  databaseURL: "https://s20260627-ea381-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "s20260627-ea381",
+  storageBucket: "s20260627-ea381.firebasestorage.app",
+  messagingSenderId: "282846563979",
+  appId: "1:282846563979:web:6e461c51d133600bbd8e8e",
+  measurementId: "G-W9VQDZV37P"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase連線成功");
